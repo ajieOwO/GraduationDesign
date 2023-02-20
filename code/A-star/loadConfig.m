03ae2917670ac54b67726c2e2970af730b1ed923
ajieOwO <ajie.wang@qq.com>
2023-02-20 14:57:27 +0800

    调整注释

--- code/A-star/loadConfig.m
+++ code/A-star/loadConfig.m
@@ -8,10 +8,10 @@ function config = loadConfig()
 	config.motionSpace.end = [1000 1000 1000];
 	% 设置运动空间范围
 	
-	config.obstaclesInfo.cubeNum = 3;			% 立方体
-	config.obstaclesInfo.sphereNum = 2;		% 球体
-	config.obstaclesInfo.cylinderNum = 3;	% 圆柱体
-	config.obstaclesInfo.size = 0.2;				% 障碍物最大尺寸
+	config.obstaclesInfo.cubeNum = 3;			% 立方体数量
+	config.obstaclesInfo.sphereNum = 2;		% 球体数量
+	config.obstaclesInfo.cylinderNum = 3;	% 圆柱体数量
+	config.obstaclesInfo.size = 0.2;			% 障碍物最大尺寸（相对于运动空间）
 	% 设置障碍物数量
 	
 	config.color.alpha = 0.7;
