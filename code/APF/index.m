670c0c984fa4a70ea5f4c2f36d9f928c7d8e016c
ajieOwO <ajie.wang@qq.com>
2023-03-06 19:59:02 +0800

    优化APF参数，使轨迹更平滑

--- code/APF/index.m
+++ code/APF/index.m
@@ -5,11 +5,11 @@ clc;
 load('../config.mat');	% 加载配置文件
 
 config.title = 'APF算法静态避障演示';
-config.apf.Ka = 1;	% 引力场正比例系数
-config.apf.m = 2;	 	% 引力势场因子
-config.apf.range = 10;	% 斥力势场范围
-config.apf.Kr = 100000;	% 斥力场正比例系数
-config.apf.step = 1;	% 寻路步长
+config.apf.Ka = 0.0001;	% 引力场正比例系数
+config.apf.m = 2;	 			% 引力势场因子
+config.apf.range = 30;	% 斥力势场范围
+config.apf.Kr = 100;		% 斥力场正比例系数
+config.apf.step = 0.01;	% 寻路步长
 
 path = findPath(config.motionRange, config.obstacles, config.apf);
 
